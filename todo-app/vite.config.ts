ae69633320d9e936780dcd88080bcaad989c8f30
takatoku23 <takutoplayer1211@icloud.com>
2025-01-13 17:14:32 +0900

    update Docker configuration for development: adjust ports and enable interactive terminal

--- todo-app/vite.config.ts
+++ todo-app/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001, // 任意のポート番号
+    host: '0.0.0.0',
+    port: 3003, // 任意のポート番号
   },
 });

98f17b5981fe1fc19f74cf009f1b41bd663b3268
takatoku23 <takutoplayer1211@icloud.com>
2025-01-13 16:10:49 +0900

    render greeting directly in index.tsx

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const App: React.FC = () => <h1>Hello, TypeScript and React!</h1>;
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(<h1>Hello, TypeScript and React!</h1>);
